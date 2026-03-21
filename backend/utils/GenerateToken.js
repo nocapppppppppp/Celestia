d783d2f62a696bd970d9432b21c3b6e77c77be00
nocapppppppppp <hanh91520@gmail.com>
2026-03-21 16:51:27 +0700

    lancuoi

--- backend/utils/GenerateToken.js
+++ backend/utils/GenerateToken.js
@@ -1,6 +1,14 @@
 require('dotenv').config()
 const jwt=require('jsonwebtoken')
 
-exports.generateToken=(payload,passwordReset=false)=>{
-    return jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:passwordReset?process.env.PASSWORD_RESET_TOKEN_EXPIRATION:process.env.LOGIN_TOKEN_EXPIRATION})
+exports.generateToken = (payload, passwordReset = false) => {
+    return jwt.sign(
+        payload,
+        process.env.JWT_SECRET, // ✅ sửa ở đây
+        {
+            expiresIn: passwordReset
+                ? process.env.PASSWORD_RESET_TOKEN_EXPIRATION
+                : process.env.LOGIN_TOKEN_EXPIRATION
+        }
+    )
 }
